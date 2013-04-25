f341cffe07c2718a1db3ccd42d9b734af45945b8
Bert Schepers <bs.schepers@gmail.com>
2013-04-25 14:21:55 +0200

    Treinkoerier textfields toegevoegd

--- src/TreinkoeriersPanel.java
+++ src/TreinkoeriersPanel.java
@@ -1,5 +1,7 @@
 
-import java.awt.BorderLayout;
+
+import java.awt.Component;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -10,12 +12,14 @@ public class TreinkoeriersPanel extends JDialog implements ActionListener{
 
     private JLabel lblTitel, lblNaam, lblAdres, lblPostcode, lblWoonplaats, lblTelnr, lblEmail, lblId, lblVerklaring;
     private JButton btnId, btnVerklaring, btnOpslaan;
+    private JTextField txtNaam, txtAdres, txtPostcode, txtWoonplaats, txtTelnr, txtEmail;
     
     public TreinkoeriersPanel(){
     
         this.setTitle("Treinkoeriers Beheren");
         this.setSize(500, 600);
-        this.setLayout(new BorderLayout());
+        this.setLayout(new GridLayout(0,4));
+        
 
         this.lblTitel = new JLabel("Treinkoerier Wijzigen");
         this.lblNaam = new JLabel("Naam:");
@@ -26,6 +30,14 @@ public class TreinkoeriersPanel extends JDialog implements ActionListener{
         this.lblEmail = new JLabel("Email:");
         this.lblId = new JLabel("Kopie ID:");
         this.lblVerklaring = new JLabel("Kopie verklaring:");
+        
+        this.txtNaam = new JTextField("");
+        this.txtAdres = new JTextField("");
+        this.txtPostcode = new JTextField("");
+        this.txtWoonplaats = new JTextField("");
+        this.txtTelnr = new JTextField("");
+        this.txtEmail = new JTextField("");
+               
         this.btnId = new JButton("Wijzigen");
         this.btnVerklaring = new JButton("Wijzigen");
         this.btnOpslaan = new JButton("Opslaan");
@@ -39,9 +51,18 @@ public class TreinkoeriersPanel extends JDialog implements ActionListener{
         this.add(lblEmail);
         this.add(lblId);
         this.add(lblVerklaring);
+        
+        this.add(txtNaam);
+        this.add(txtAdres);
+        this.add(txtPostcode);
+        this.add(txtWoonplaats);
+        this.add(txtTelnr);
+        this.add(txtEmail);
+                
         this.add(btnId);
         this.add(btnVerklaring);
         this.add(btnOpslaan);
+        
         
         this.setVisible(true);
     }
